a518d392686b7aa775e0ec862ee9da624797f053
Fahad Qazi <fahadqazi@gmail.com>
2017-06-29 22:43:53 +0100

    refactor and rewrite readme

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,24 +1,17 @@
-const ce = React.createElement
+const ce = React.createElement;
 
-const MyTitle = function(props){
-  return(
-    ce('div', null,
-      ce('h1', { color: props.color }, props.title)
-    )
-  )
-}
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { color: props.color }, props.title));
+};
 
-const MyFirstComponent = function(){
-  return(
-    ce('div', {id: 'my-first-component'},
-      ce(MyTitle, {title: 'game of thrones', color:'aquamarine'}),
-      ce(MyTitle, {title: 'breaking bad', color: 'orange'}),
-      ce(MyTitle, {title: 'narcos', colors: 'yellow'})
-    )
-  )
-}
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'game of thrones', color: 'aquamarine' }),
+    ce(MyTitle, { title: 'breaking bad', color: 'orange' }),
+    ce(MyTitle, { title: 'narcos', colors: 'yellow' })
+  );
+};
 
-ReactDom.render(
-  ce(MyFirstComponent),
-  document.getElementById('app')
-)
+ReactDom.render(ce(MyFirstComponent), document.getElementById('app'));
