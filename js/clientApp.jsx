515fccf7769ddacfa2cb0e6e7945ec46ca27025a
Fahad Qazi <fahadqazi@gmail.com>
2017-06-30 23:45:51 +0100

    adding 404 route and use Switch

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './landing';
 import Search from './search';
 
+const FourohFour = () => <h1>404</h1>;
+
 const App = () =>
-  <HashRouter>
+  <BrowserRouter>
     <div className="app">
-      <Route exact path="/" component={Landing} />
-      <Route path="/search" component={Search} />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourohFour} />
+      </Switch>
     </div>
-  </HashRouter>;
+  </BrowserRouter>;
 
 ReactDom.render(<App />, document.getElementById('app'));
--- js/landing.jsx
+++ js/landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () =>
   <div className="landing">
     <h1>svideo</h1>
     <input type="text" placeholder="search" />
-    <a href="">Browse All</a>
+    <Link to="/search">Browse All</Link>
   </div>;
 
 export default Landing;
