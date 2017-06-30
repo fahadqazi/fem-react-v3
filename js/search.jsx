d5dd7ddf2d9f6d10cb21334da6d1b4602ef9e5ae
Fahad Qazi <fahadqazi@gmail.com>
2017-07-01 00:52:10 +0100

    styled component image

--- js/search.jsx
+++ js/search.jsx
@@ -4,9 +4,11 @@ import preload from '../data.json';
 
 const Search = () =>
   <div className="search">
+    <div>
     {preload.shows.map(show =>
       <ShowCard key={show.imdbID} {...show}/>
     )}
+    </div>
   </div>;
 
 export default Search;
