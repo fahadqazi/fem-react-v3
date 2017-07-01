ebdcf636411d87937942346d127f403e706bbdd2
Fahad Qazi <fahadqazi@gmail.com>
2017-07-01 21:31:18 +0100

    adding new tests

--- js/__tests__/search.spec.jsx
+++ js/__tests__/search.spec.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import preload from '../../data.json';
+import ShowCard from '../showCard';
 import Search from '../search';
 
 test('Search renders correctly', () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
 });
+
+test('Search should render correct amount of ShowCards', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+})
