c1eb7beb6de226fe74e9d1089053a37d80fd94d8
Fahad Qazi <fahadqazi@gmail.com>
2017-07-01 21:49:29 +0100

    test for Search page

--- js/__tests__/search.spec.jsx
+++ js/__tests__/search.spec.jsx
@@ -12,4 +12,17 @@ test('Search renders correctly', () => {
 test('Search should render correct amount of ShowCards', () => {
   const component = shallow(<Search />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
-})
+});
+
+test('Search should render correct number of shows based on search term', () => {
+  const component = shallow(<Search />);
+  const searchWord = 'black';
+  component.find('input').simulate('change', { target: { value: searchWord } });
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0,
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
