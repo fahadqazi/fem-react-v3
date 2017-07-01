8cc80e950592c44070cc157d1947324a8cce1ac7
Fahad Qazi <fahadqazi@gmail.com>
2017-07-01 01:12:52 +0100

    start using state, on change handler added on input

--- js/search.jsx
+++ js/search.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './showCard';
 import preload from '../data.json';
 
-const Search = () =>
-  <div className="search">
-    <div>
-    {preload.shows.map(show =>
-      <ShowCard key={show.imdbID} {...show}/>
-    )}
-    </div>
-  </div>;
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'this is some type of bug statement',
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
 
+  handleSearchTermChange(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
