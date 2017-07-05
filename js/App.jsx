966bcd29bece7b1b00163b80f81db8a7bcbb4d0f
Fahad Qazi <fahadqazi@gmail.com>
2017-07-05 20:23:12 +0100

    adding details rotue with a function passed into component prop

--- js/App.jsx
+++ js/App.jsx
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
+import preload from '../data.json';
 import Landing from './landing';
 import Search from './search';
 import Details from './details';
@@ -13,7 +15,15 @@ const App = () =>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route path="/details/:id" component={Details}/>
+        <Route
+          path="/details/:id"
+          component={(props: {match: Match}) => {
+            const selectedShow = preload.shows.find(
+              show => props.match.params.id === show.imdbID,
+            );
+            return <Details show={selectedShow} {...props}/>;
+          }}
+        />
         <Route component={FourohFour} />
       </Switch>
     </div>
